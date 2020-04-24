1d8db3531fac0d800332c9ef11f92838b3833c17
Marco Starker <marco.starker@gmx.net>
2020-04-24 13:30:25 +0200

    dep update

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve';
-import progress from 'rollup-plugin-progress';
+import resolve from '@rollup/plugin-node-resolve';
 import strip from 'rollup-plugin-strip';
 import babel from 'rollup-plugin-babel';
 import {terser} from "rollup-plugin-terser";
@@ -9,31 +8,29 @@ import clear from "rollup-plugin-clear";
 export default [
     // Node Module
     {
-        input:   './src/assert.js',
-        output:  {
-            file:   './dist/index.js',
+        input: './src/assert.js',
+        output: {
+            file: './dist/index.js',
             format: 'cjs',
-            name:   'assert',
+            name: 'assert',
         },
         plugins: [
             resolve(),
             clear({targets: ['./dist']}),
-            progress({clearLine: false}),
         ]
     },
     // ES2015 Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/assert.min.js',
-            format:    'iife',
-            name:      'assert',
-            compact:   true,
+        input: './src/index.js',
+        output: {
+            file: './dist/assert.min.js',
+            format: 'iife',
+            name: 'assert',
+            compact: true,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress({clearLine: false}),
             strip({
                 // set this to `false` if you don't want to remove debugger statements
                 debugger: true,
@@ -42,41 +39,40 @@ export default [
                 sourceMap: true,
             }),
             babel({
-                exclude:         'node_modules/**',
-                runtimeHelpers:  false,
+                exclude: 'node_modules/**',
+                runtimeHelpers: false,
                 externalHelpers: false,
-                babelrc:         false,
-                presets:         [
+                babelrc: false,
+                presets: [
                     [
                         "@babel/env",
                         {
                             targets: {
-                                ie:      '11',
-                                edge:    '17',
+                                ie: '11',
+                                edge: '17',
                                 firefox: '60',
-                                chrome:  '71',
-                                safari:  '11.1',
+                                chrome: '71',
+                                safari: '11.1',
                             },
                         }
                     ]
                 ],
             }),
-            terser({sourcemap: true}),
+            terser(),
         ]
     },
     // ES2015 None-Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/assert.js',
-            format:    'iife',
-            name:      'assert',
-            compact:   false,
+        input: './src/index.js',
+        output: {
+            file: './dist/assert.js',
+            format: 'iife',
+            name: 'assert',
+            compact: false,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress({clearLine: false}),
             strip({
                 // set this to `false` if you don't want to remove debugger statements
                 debugger: true,
@@ -85,20 +81,20 @@ export default [
                 sourceMap: true,
             }),
             babel({
-                exclude:         'node_modules/**',
-                runtimeHelpers:  false,
+                exclude: 'node_modules/**',
+                runtimeHelpers: false,
                 externalHelpers: false,
-                babelrc:         false,
-                presets:         [
+                babelrc: false,
+                presets: [
                     [
                         "@babel/env",
                         {
                             targets: {
-                                ie:      '11',
-                                edge:    '17',
+                                ie: '11',
+                                edge: '17',
                                 firefox: '60',
-                                chrome:  '71',
-                                safari:  '11.1',
+                                chrome: '71',
+                                safari: '11.1',
                             },
                         }
                     ]
@@ -108,17 +104,16 @@ export default [
     },
     // ES Module Minified
     {
-        input:   './src/assert.js',
-        output:  {
-            file:      './dist/assert.esm.min.js',
-            format:    'esm',
-            name:      'assert',
-            compact:   true,
+        input: './src/assert.js',
+        output: {
+            file: './dist/assert.esm.min.js',
+            format: 'esm',
+            name: 'assert',
+            compact: true,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress({clearLine: false}),
             strip({
                 // set this to `false` if you don't want to remove debugger statements
                 debugger: true,
@@ -126,22 +121,21 @@ export default [
                 // set this to `false` if you're not using sourcemaps – defaults to `true`
                 sourceMap: true,
             }),
-            terser({sourcemap: true}),
+            terser(),
         ]
     },
     // ES Module None-Minified
     {
-        input:   './src/assert.js',
-        output:  {
-            file:      './dist/assert.esm.js',
-            format:    'esm',
-            name:      'assert',
-            compact:   false,
+        input: './src/assert.js',
+        output: {
+            file: './dist/assert.esm.js',
+            format: 'esm',
+            name: 'assert',
+            compact: false,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress({clearLine: false}),
             strip({
                 // set this to `false` if you don't want to remove debugger statements
                 debugger: true,
